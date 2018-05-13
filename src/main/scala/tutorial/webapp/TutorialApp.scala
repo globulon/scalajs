9c1cd950d4f71ed0706fc8946552d9fc5682db2e
Marc-Daniel Ortega <globulon@gmail.com>
2018-05-13 14:00:04 +0100

    [IMP] Activate JQuery on UI level

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,25 +1,16 @@
 package tutorial.webapp
-import org.scalajs.dom
-import dom.document
-import org.scalajs.dom.raw.Node
-
-import scala.scalajs.js.annotation.JSExportTopLevel
+import org.querki.jquery._
 
 object TutorialApp {
-  private def appendPar(targetNode: dom.Node, text: String): Node = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
+  private def addClickedMessage(): JQuery = $("body").append("<p>You clicked me !</p>")
 
-  @JSExportTopLevel("addClickedMessage")
-  def addClickedMessage(): Node = {
-    appendPar(document.body, "You clicked the button!")
+  private def setupUI(): JQuery = {
+    $("body").append("<p>Hello World</p>")
+    $("#click-me-button").click(() ⇒ addClickedMessage())
   }
 
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
+    $(() ⇒ setupUI())
     ()
   }
 }
